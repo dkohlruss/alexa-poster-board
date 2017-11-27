35e7e5f384717c01e2085a33dda6b5ef94ff587f
dkohlruss <dkohlruss@gmail.com>
2017-11-27 11:21:39 -0700

    updated amazon app id

--- lambda/constants/constants.js
+++ lambda/constants/constants.js
@@ -1,25 +1,25 @@
 var constants = Object.freeze({
-  //App ID
-  appId: 'amzn1.ask.skill.5d7df2d1-6622-4bdd-be5a-02283739cdb1',
+	//App ID
+	appId: 'amzn1.ask.skill.684a7ced-a776-4011-afef-b7ee65c52610',
 
-  // DynamoDB Table name
-  dynamoDBTableName: 'Confessions',
-  confessionDBTableName: 'ConfessionList',
+	// DynamoDB Table name
+	dynamoDBTableName: 'Confessions',
+	confessionDBTableName: 'ConfessionList',
 
-  // SkilL States
-  states: {
-    ONBOARDING: '',
-    TUTORIAL: '_TUTORIAL',
-    MAIN: '_MAIN',
-    TUTCONFESS: '_TUTORIAL_CONFESS',
-    TUTRECORDING: '_TUTORIAL_RECORDING',
-    RECORDING: '_RECORDING',
-    LISTENING: '_LISTENING',
-    VOTING: '_VOTING',
-    SAVING: '_SAVING'
-  },
+	// SkilL States
+	states: {
+		ONBOARDING: '',
+		TUTORIAL: '_TUTORIAL',
+		MAIN: '_MAIN',
+		TUTCONFESS: '_TUTORIAL_CONFESS',
+		TUTRECORDING: '_TUTORIAL_RECORDING',
+		RECORDING: '_RECORDING',
+		LISTENING: '_LISTENING',
+		VOTING: '_VOTING',
+		SAVING: '_SAVING'
+	},
 
-  ALL_ADDRESS_PERMISSION: ['read::alexa:device:all:address']
+	ALL_ADDRESS_PERMISSION: ['read::alexa:device:all:address']
 });
 
 module.exports = constants;
